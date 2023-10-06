2cb87ceb44b2fcef2cc765cc8cfd9af0b120eef9
Muhammad Syafiq <fiqrapngai77@gmail.com>
2023-10-07 00:26:06 +0800

    Updated for postgresql

--- knexfile.js
+++ knexfile.js
@@ -6,13 +6,14 @@ dotenv.config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 const knexConfig = {
-  client: "mysql2",
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
-    port: 3306,
+    port: 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
   },
 
   migrations: {
